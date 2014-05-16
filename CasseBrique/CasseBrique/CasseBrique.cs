a91da347ecd82fcd33664e375ac72c824f1d218b
ScaY <stephane.viper@gmail.com>
2014-05-16 10:41:25 +0200

    Création du projet

---
+++ CasseBrique/CasseBrique/CasseBrique.cs
@@ -0,0 +1,148 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace CasseBrique
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class CasseBrique : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Texture2D barImgMid;   //image représentant la barre du casse brique
+        private Texture2D ballImgMid;  //image représentant la balle du jeu
+
+        private Vector2 positionBar;    //vecteur représentant la position de la barre
+        private Vector2 positionBall;   //vecteur représentant la position de la balle
+
+        private Vector2 directionBall;    //vecteur représentant la direction de déplacement de la balle
+
+        public const float speedBall = 0.3f;        //constante représentant la vitesse de la balle
+
+        private SpriteFont font;                    //variable permettant d'écrire
+
+        private Vector2 positionBrickInfo;          //vecteur repésentant la position de l'information des briques restantes 
+
+        //dimensions de la fenêtre
+        private int widthFrame;
+        private int heightFrame;
+
+        //parametre de la position de la barre
+        public const int xSetBar = 5;
+        public const int yPosition = 2;
+
+        public CasseBrique()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            widthFrame = Window.ClientBounds.Width;
+            heightFrame = Window.ClientBounds.Height;
+
+            directionBall = Vector2.Normalize(Vector2.UnitX);
+
+            positionBall = Vector2.Zero;
+            positionBar = Vector2.Zero;
+
+            positionBrickInfo = new Vector2(widthFrame - 100, 0);   //é changer @@@@ !!!
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //chargement de l'image de la barre du casse brique
+            barImgMid = Content.Load<Texture2D>("horizontalBarMid");
+
+            //chargement de l'image de la balle du jeu
+            ballImgMid = Content.Load<Texture2D>("balleMid");
+
+            //chargement du style d'écriture
+            font = Content.Load<SpriteFont>("Default");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (positionBall.X >= widthFrame)
+            {
+                directionBall = Vector2.Normalize(new Vector2(-1, 0));
+            }
+            else if (positionBall.X <= 0)
+            {
+                directionBall = Vector2.Normalize(Vector2.UnitX);
+            }
+
+            //permet de gérer le problème du framerate (nombre de tours de la "boule de jeu")
+            positionBall += directionBall * speedBall * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            if (positionBar == Vector2.Zero)
+            {
+                positionBar = new Vector2((widthFrame - barImgMid.Width) / yPosition, heightFrame - ballImgMid.Height * xSetBar);
+
+            }
+            //signal au SpriteBatch le début du déssin
+            spriteBatch.Begin();
+            spriteBatch.Draw(barImgMid, positionBar, Color.White);
+            spriteBatch.Draw(ballImgMid, positionBall, Color.White);
+            spriteBatch.DrawString(font, "Life remaining: ", positionBrickInfo, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
