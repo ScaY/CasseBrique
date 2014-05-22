7300a6d61d1b35b1408250765d76b27e191240ec
SquallB <mazento2002@yahoo.fr>
2014-05-22 11:41:59 +0200

    ajout fichier view bonus

---
+++ CasseBrique/CasseBrique/Views/ViewBonus.cs
@@ -0,0 +1,19 @@
+﻿using Breakout.Bonus;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Breakout.Views
+{
+    public class ViewBonus : ShapeView
+    {
+        public AbstractBonus Bonus { get; set; }
+
+        public ViewBonus(AbstractBonus bonus, Texture2D texture) : base(bonus, texture)
+        {
+        }
+    }
+}
