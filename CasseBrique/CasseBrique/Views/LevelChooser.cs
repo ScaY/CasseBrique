e86aa435376771015571a0e47d9891f1e1e496e0
HugoDufossez <hdu.web@gmail.com>
2014-05-22 10:24:29 +0200

    add views

---
+++ CasseBrique/CasseBrique/Views/LevelChooser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CasseBrique.Views
+{
+    public partial class LevelChooser : Form
+    {
+        public LevelChooser()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            using (var game = new GameXNA())
+             game.Run();
+        }
+    }
+}
