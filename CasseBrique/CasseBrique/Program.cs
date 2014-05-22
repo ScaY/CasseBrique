ff9a4da1eca4be939ea38e163e687a4b67c87b3a
Moumoush <mehdi.bouchagour@laposte.net>
2014-05-22 13:26:45 +0200

    updating Menu

--- CasseBrique/CasseBrique/Program.cs
+++ CasseBrique/CasseBrique/Program.cs
@@ -1,9 +1,9 @@
 ﻿#region Using Statements
 using Breakout.Views;
+using CasseBrique.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
 #endregion
 
 namespace Breakout
@@ -20,7 +20,7 @@ namespace Breakout
         [STAThread]
         static void Main()
         {
-            Menu m = new Menu();
+                Menu m = new Menu();
                 using (var game = new GameXNA())
                     game.Run();
             
