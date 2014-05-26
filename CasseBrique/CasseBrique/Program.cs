a7f1f8b79380ae991198b89c4874e6efd983dc4b
Moumoush <mehdi.bouchagour@laposte.net>
2014-05-26 11:20:08 +0200

    changing Programme.cs

--- CasseBrique/CasseBrique/Program.cs
+++ CasseBrique/CasseBrique/Program.cs
@@ -1,9 +1,9 @@
 ﻿#region Using Statements
 using Breakout.Views;
-using CasseBrique.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 #endregion
 
 namespace Breakout
@@ -20,9 +20,10 @@ namespace Breakout
         [STAThread]
         static void Main()
         {
-                //Menu m = new Menu();
-                using (var game = new GameXNA(null))
-                    game.Run();
+                Home m = new Home();
+                Application.Run(m);
+                //using (var game = new GameXNA())
+                    //game.Run();
             
         }
     }
