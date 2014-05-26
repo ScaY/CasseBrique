f0bc5925a9156d3b37207cdaeed76af666445d80
SquallB <mazento2002@yahoo.fr>
2014-05-26 15:32:35 +0200

    ajout des events

---
+++ CasseBrique/CasseBrique/Events/AddedBonusEvent.cs
@@ -0,0 +1,20 @@
+﻿using Breakout.Bonus;
+using Breakout.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Breakout.Events
+{
+    public class AddedBonusEvent : BonusEvent
+    {
+        public AddedBonusEvent()
+        {
+        }
+
+        public AddedBonusEvent(AbstractModel model, AbstractBonus bonus) : base(model, bonus)
+        {
+        }
+    }
+}
---
+++ CasseBrique/CasseBrique/Events/RemovedBrickEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Breakout.Model;
+
+namespace Breakout.Events
+{
+    public class RemovedBrickEvent : BrickEvent
+    {
+        public RemovedBrickEvent()
+        {
+        }
+
+        public RemovedBrickEvent(AbstractModel model, Brick brick) : base(model, brick)
+        {
+        }
+    }
+}
