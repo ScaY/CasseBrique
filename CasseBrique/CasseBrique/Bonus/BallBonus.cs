fea61646248d91ed0ab25788be3e626a76c6774e
SquallB <mazento2002@yahoo.fr>
2014-05-27 20:43:14 +0200

    ajout des fichiers de bonus

---
+++ CasseBrique/CasseBrique/Bonus/BallBonus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Breakout.Bonus
+{
+    public abstract class BallBonus : AbstractBonus
+    {
+        public BallBonus(float modifier, int duration) : base(modifier, duration)
+        {
+        }
+    }
+}
---
+++ CasseBrique/CasseBrique/Bonus/BallSpeedBonus.cs
@@ -0,0 +1,30 @@
+﻿using Breakout.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Breakout.Bonus
+{
+    public class BallSpeedBonus : BallBonus
+    {
+        public override void ApplyBonus(Model.BreakoutModel model, Player player)
+        {
+            foreach(Ball ball in model.Balls) {
+                ball.Speed += Modifier;
+            }
+        }
+
+        public override void RemoveBonus(BreakoutModel model, Player player)
+        {
+            foreach (Ball ball in model.Balls)
+            {
+                ball.Speed -= Modifier;
+            }
+        }
+
+        public BallSpeedBonus(float modifier, int duration) : base(modifier, duration)
+        {
+        }
+    }
+}
