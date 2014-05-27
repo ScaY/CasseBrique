4f8ae9f3b20eec9415fe5735cb5dcc412bcddd3a
SquallB <mazento2002@yahoo.fr>
2014-05-27 16:41:53 +0200

    rien

--- CasseBrique/CasseBrique/Program.cs
+++ CasseBrique/CasseBrique/Program.cs
@@ -21,7 +21,7 @@ namespace Breakout
         [STAThread]
         static void Main()
         {
-                Home m = new Home();
+                Home m = new NewHome();
                 Application.Run(m);
                // using (var game = new GameXNA(new Player("toto", null)))
                // game.Run();
