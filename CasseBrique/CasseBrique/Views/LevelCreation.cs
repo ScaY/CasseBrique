aae7d4bddb635276643271f8cc4bd7c66adf624e
Moumoush <mehdi.bouchagour@laposte.net>
2014-05-30 12:09:50 +0200

    bonus raisonnables

--- CasseBrique/CasseBrique/Views/LevelCreation.cs
+++ CasseBrique/CasseBrique/Views/LevelCreation.cs
@@ -364,15 +364,15 @@ namespace CasseBrique.Views
                 double randomBonus = Math.Floor(new Random().NextDouble() * 3);
                 if (randomBonus == 0)
                 {
-                    this.bonus = new BallSpeedBonus((float)10, 15);
+                    this.bonus = new BallSpeedBonus((float)0.05, 15);
                 }
                 else if (randomBonus == 1)
                 {
-                    this.bonus = new BarSizeBonus((float)10, 15);
+                    this.bonus = new BarSizeBonus((float)0.5, 15);
                 }
                 else if (randomBonus == 2)
                 {
-                    this.bonus = new AddBallBonus((float)10, 15);
+                    this.bonus = new AddBallBonus((float)0.5, 15);
                 }
                 
                 
