d4267a745a5553c331924063f8bc6389d29802d5
ScaY <stephane.viper@gmail.com>
2014-05-30 11:59:09 +0200

    MaJ ballBonus

--- CasseBrique/CasseBrique/Views/ViewBreakout.cs
+++ CasseBrique/CasseBrique/Views/ViewBreakout.cs
@@ -27,6 +27,7 @@ namespace Breakout.Views
         private Texture2D textureBall;
         private Texture2D textureBonus;
         private Texture2D texturePause;
+        private Texture2D textureBallBonus;
 
         public ViewBreakout(BreakoutModel breakout,int height,int width)
         {   
@@ -61,6 +62,8 @@ namespace Breakout.Views
             this.textureBall = content.Load<Texture2D>("ballSmall");
             this.textureBonus = content.Load<Texture2D>("bonus");
             this.texturePause = content.Load<Texture2D>("pause");
+            this.textureBallBonus = content.Load<Texture2D>("ballBonus");
+
             foreach (ViewBar viewBar in this.ViewBars)
             {
                 viewBar.Texture = this.textureBar;
@@ -154,7 +157,7 @@ namespace Breakout.Views
                 if (e is AddedBallEvent)
                 {
                     ViewBall viewBall = new ViewBall(be.Ball);
-                    viewBall.Texture = this.textureBall;
+                    viewBall.Texture = this.textureBallBonus;
                     this.ViewBalls.Add(viewBall);
                 }
                 else if (e is RemovedBallEvent)
